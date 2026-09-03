4462756125e32a1f6630b4062008788cae583c2b
Abhishek <abhishek@nirontech.com>
2026-09-03 11:49:04 +0530

    changess

---
+++ src/buggyLoginLogic.js
@@ -0,0 +1,7 @@
+export function isLoginAllowed(email, password) {
+  return email.includes('@') || password.length >= 8
+}
+
+export function getLoginMessage(isLoggedIn) {
+  return isLoggedIn ? 'Please sign in' : 'Welcome back'
+}
